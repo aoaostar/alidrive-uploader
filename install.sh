b815c19809839c7ee429f1f69a21cc6a9a0cd4ff
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-04-26 22:44:58 +0800

    🔨 安装脚本新增代理加速

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
 hash tar uname grep curl head
 
 OS="$(uname)"
+PROXY="https://proxy.aoaostar.workers.dev/"
+
 case $OS in
   Linux)
     OS='linux'
@@ -38,10 +39,33 @@ case $ARCH in
 esac
 
 
+if [[ $1 == "-y" ]]; then
+  echo "使用加速代理"
+elif [[ $1 == "-n" ]]; then
+  echo "不使用加速代理"
+  PROXY=""
+elif [[ $(curl -m 10 -s https://ipapi.co/json | grep 'China') != "" ]]; then
+  echo "根据ipapi.co提供的信息，当前IP可能在中国"
+  read -e -r -p "是否选用使用加速代理完成安装? [Y/n] " input
+  case $input in
+  [yY][eE][sS] | [yY])
+    echo "使用加速代理"
+    ;;
+
+  [nN][oO] | [nN])
+    echo "不使用加速代理"
+    PROXY=""
+    ;;
+  *)
+    echo "使用加速代理"
+    ;;
+  esac
+fi
+
 Install()
 {
 	echo '正在下载阿里云盘上传工具...'
-  CORE_DOWNLOAD_URL=$(curl -fsSL https://api.github.com/repos/aoaostar/alidrive-uploader/releases/latest | grep "browser_download_url.*$OS.*$ARCH" | cut -d '"' -f 4)
+  CORE_DOWNLOAD_URL=$PROXY$(curl -fsSL "$PROXY"https://api.github.com/repos/aoaostar/alidrive-uploader/releases/latest | grep "browser_download_url.*$OS.*$ARCH" | cut -d '"' -f 4)
   curl -L "$CORE_DOWNLOAD_URL" | tar -xz
 	#==================================================================
 	echo '================================================'
